1210732d41182b3d0a0cb3b45b3fa1ab15c57aa1
dalessi <danialessi93@hotmail.com>
2025-03-19 19:36:41 +0100

    user data added to i4

--- modules/i4/output.tf
+++ modules/i4/output.tf
@@ -1,5 +1,5 @@
 output "instance_public_ip" {
-  value       = aws_instance.public_ec2.public_ip
+  value = aws_instance.public_ec2.public_ip
 }
 output "instance_private_ip" {
   value = aws_instance.public_ec2.private_ip
