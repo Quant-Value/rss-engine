44e2d30513ed54ab92cb67b1b9415fa18cc7ccc9
dalessi <danialessi93@hotmail.com>
2025-03-24 20:24:43 +0100

    changes to sg ports

--- modules/grafana_frontend/output.tf
+++ modules/grafana_frontend/output.tf
@@ -4,3 +4,7 @@ output "instance_public_ip" {
 output "instance_private_ip" {
   value = aws_instance.public_ec2.private_ip
 }
+
+output "sg_id" {
+  value = aws_security_group.sg.id  
+}
--- modules/grafana_frontend/sg.tf
+++ modules/grafana_frontend/sg.tf
@@ -8,18 +8,18 @@ resource "aws_security_group" "sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+  # ingress {
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
     ingress {
     from_port   = 3000
     to_port     = 3000
