e3c569a7ac1221c55238ba4de95783f1ccc4988d
Salvadortarrio <salvador.tarrio@campusdual.com>
2025-03-25 18:23:12 +0100

    sg server modfification

--- modules/sw_server/sg.tf
+++ modules/sw_server/sg.tf
@@ -12,8 +12,8 @@ resource "aws_security_group" "sg_server" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    #self=true
+    #cidr_blocks = ["0.0.0.0/0"]
+    self=true
   }
     ingress {
     from_port   = -1
